d5cfb08b06b1a578205c7b684b19f555309233e6
Gwymlas <smrszr63@gmail.com>
2022-12-16 23:24:58 +0400

    MPI

--- main.cpp
+++ main.cpp
@@ -1,132 +1,102 @@
 #include <iostream>
-#include <vector>
-#include <fstream>
-#include <ctime>
+#include "mpi.h"
 
 
-std::vector<std::vector<double>> MatrixMultiplication(const std::vector<std::vector<double>>& first_matrix,
-                                                      const std::vector<std::vector<double>>& second_matrix){
-    // (n,m)*(m,k) = (n,k)
-    // count rows = n
-    // count columns = k
-    if (first_matrix[0].size() != second_matrix.size()) throw std::exception();
+int main(int argc,char *argv[])
+{
+    double start, stop;
+    int i, j, k, l;
+    int *A, *B, *C, *buffer, *ans;
+    int size = 1000;
+    int rank, numprocs, line;
 
-    size_t count_rows = first_matrix.size();
-    size_t count_columns = second_matrix[0].size();
-    std::vector<std::vector<double>> result(count_rows);
-    for (int i = 0; i < count_rows; ++i) {
-        result[i].resize(count_columns);
-    }
+    MPI_Init(&argc,&argv); //MPI Initialize
+    MPI_Comm_rank(MPI_COMM_WORLD,&rank); // Получить текущий номер процесса
+    MPI_Comm_size(MPI_COMM_WORLD,&numprocs); // Получить количество процессов
 
-    size_t m = second_matrix.size();
+    line = size/numprocs; // Делим данные на блоки (количество процессов), и основной процесс также должен обрабатывать данные
+    A = new int[size*size];
+    B = new int[size*size];
+    C = new int[size*size];
+    // Размер кеша больше или равен размеру обрабатываемых данных, когда он больше, чем фактическая часть данных
+    buffer = new int[size*line]; // Размер пакета данных
+    ans = new int[size*line]; // Сохраняем результат расчета блока данных
 
-    for (int i = 0; i < count_rows; ++i) {
-        for (int j = 0; j < count_columns; ++j) {
-            for (int k = 0; k < m; ++k) {
-                result[i][j] += first_matrix[i][k] * second_matrix[k][j];
+    // Основной процесс присваивает матрице начальное значение и передает матрицу N каждому процессу, а матрицу M передает каждому процессу в группах.
+    if (rank == 0)
+    {
+        for(i = 0; i < size; i++) // Чтение данных
+            for(j = 0; j < size; j++)
+                A[i * size + j] = (i + 2) * (j + 1);
+
+        for(i = 0; i < size; i++)
+            for(j = 0; j < size; j++)
+                B[i * size + j] = (i + 1) + 2 * (j + 3);
+
+        start = MPI_Wtime();
+        // Отправить матрицу N другим подчиненным процессам
+        for (i = 1; i < numprocs; i++) {
+            MPI_Send(B,size*size,MPI_INT,i,0,MPI_COMM_WORLD);
+        }
+        // Отправляем каждую строку a каждому подчиненному процессу по очереди
+        for (l = 1; l < numprocs; l++) {
+            MPI_Send(A+(l-1)*line*size,size*line,MPI_INT,l,1,MPI_COMM_WORLD);
+        }
+        // Получаем результат, рассчитанный по процессу
+        for (k = 1; k < numprocs; k++) {
+            MPI_Recv(ans,line*size,MPI_INT,k,3,MPI_COMM_WORLD,MPI_STATUS_IGNORE);
+            // Передаем результат в массив c
+            for (i = 0; i < line; i++) {
+                for (j = 0; j < size; j++) {
+                    C[((k-1)*line+i)*size+j] = ans[i*size+j];
+                }
             }
         }
-    }
-
-    return result;
-}
-
-
-std::vector<std::vector<double>> CreateRandomMatrix(size_t count_rows, size_t count_columns) {
-    std::vector<std::vector<double>> result(count_rows);
-    for (auto& i: result) {
-        i.resize(count_columns);
-    }
-
-    for (int i = 0; i < count_rows; ++i) {
-        for (int j = 0; j < count_columns; ++j) {
-            result[i][j] = rand() % 10;
-        }
-    }
-
-    return result;
-}
-
-
-void CreateFile(const std::vector<std::vector<double>>& matrix, const char* path) {
-    std::ofstream out;
-    out.open(path);
-    if (out.is_open()) {
-        std::cout << "OK" << std::endl;
-        for (int i = 0; i < matrix.size(); ++i) {
-            for (int j = 0; j < matrix[i].size(); ++j) {
-                out << std::setw(10) << matrix[i][j];
-            }
-            out << std::endl;
-        }
-    }
-    out.close();
-}
-
-
-void ReadFile(std::vector<std::vector<double>>& matrix, size_t count_rows, size_t count_columns, const char* path) {
-    matrix.resize(count_rows);
-    for (auto& elem: matrix) {
-        elem.resize(count_columns);
-    }
-    std::ifstream in;
-    in.open(path);
-    if (in.is_open()) {
-        std::cout << "OK" << std::endl;
-        for (int i = 0; i < matrix.size(); ++i) {
-            for (int j = 0; j < matrix[i].size(); ++j) {
-                in  >> matrix[i][j];
+        // Рассчитать оставшиеся данные
+        for (i = (numprocs-1) * line;i < size; i++) {
+            for (j = 0; j < size; j++) {
+                int temp=0;
+                for (k = 0; k < size; k++)
+                    temp += A[i*size+k]*B[k*size+j];
+                C[i*size+j] = temp;
             }
         }
+
+        // Результат теста
+        // Статистика по времени
+        stop = MPI_Wtime();
+
+        std::cout << "Time: " << stop-start << std::endl;
+
+        //Print_Matrix(C, size);
+
+        delete [] A;
+        delete [] B;
+        delete [] C;
+        delete [] buffer;
+        delete [] ans;
     }
-    in.close();
-}
 
+        // Другие процессы получают данные и после вычисления результата отправляют их в основной процесс
+    else {
+        // Получаем широковещательные данные (матрица b)
+        MPI_Recv(B,size*size,MPI_INT,0,0,MPI_COMM_WORLD,MPI_STATUS_IGNORE);
 
-void PrintMatrix(const std::vector<std::vector<double>>& matrix) {
-    for (int i = 0; i < matrix.size(); ++i) {
-        for (int j = 0; j < matrix[i].size(); ++j) {
-            std::cout << matrix[i][j] << " ";
+        MPI_Recv(buffer,size*line,MPI_INT,0,1,MPI_COMM_WORLD,MPI_STATUS_IGNORE);
+        // Рассчитать результат продукта и отправить результат в основной процесс
+        for (i = 0; i < line; i++) {
+            for (j = 0; j < size; j++) {
+                int temp=0;
+                for(k = 0; k < size; k++)
+                    temp += buffer[i*size+k]*B[k*size+j];
+                ans[i*size+j]=temp;
+            }
         }
-        std::cout << std::endl;
+        // Отправить результат расчета в основной процесс
+        MPI_Send(ans,line*size,MPI_INT,0,3,MPI_COMM_WORLD);
     }
-}
 
-
-int main() {
-    srand(time(0));
-
-    size_t n = 1500;
-    size_t m = 1500;
-    size_t k = 1500;
-
-
-    auto first = CreateRandomMatrix(n, m);
-    auto second = CreateRandomMatrix(m, k);
-
-    CreateFile(first, "/Users/gwymlas/Desktop/1.txt");
-    CreateFile(second, "/Users/gwymlas/Desktop/2.txt");
-
-    //std::vector<std::vector<double>> first, second;
-
-    //ReadFile(first, n, m, "/Users/gwymlas/Desktop/1.txt");
-    //ReadFile(second, m, k, "/Users/gwymlas/Desktop/2.txt");
-
-    clock_t start = clock();
-
-    auto result = MatrixMultiplication(first, second);
-    clock_t end = clock();
-
-    CreateFile(result, "/Users/gwymlas/Desktop/result.txt");
-
-
-
-    //PrintMatrix(result);
-
-    std::cout << "Time of multiplication: " << (double) (end-start) / CLOCKS_PER_SEC << std::endl;
-    std::cout << "First matrix: ("  << n << ", " << m << ")" << std::endl;
-    std::cout << "Second matrix: (" << m << ", " << k << ")"<< std::endl;
-    std::cout << "Result matrix: (" << n << ", " << k << ")" << std::endl;
+    MPI_Finalize(); // Конец
 
     return 0;
 }
